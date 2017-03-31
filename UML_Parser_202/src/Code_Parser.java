a407ed3aea0cf8b5dd27cdd3db0b6b52ec19267d
Avdeep2802 <Avdeep2802@gmail.com>
2017-03-30 23:55:43 -0700

    parser

---
+++ UML_Parser_202/src/Code_Parser.java
@@ -0,0 +1,75 @@
+import java.util.HashMap;
+import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+
+public class Code_Parser {
+	
+	private StringBuilder build_uml;
+	private HashMap<String,ClassOrInterfaceDeclaration> CIMap;
+	
+	public Code_Parser()
+	{
+		build_uml = new StringBuilder();		
+	}
+
+	public StringBuilder getBuild_uml() {
+		return build_uml;
+	}
+
+	public StringBuilder Read(String path) throws FileNotFoundException
+	{
+		File file = new File(path);
+		build_uml.append("@startuml\n skinparam classAttributeIconSize 0\n");
+		System.out.println();
+		if(file.exists())
+		{
+			System.out.println("file exists");
+			System.out.println(file.listFiles().toString());
+			for(File f : file.listFiles())
+			{
+				if(f.getName().endsWith(".java"))
+				{
+					try 
+					{
+						System.out.println("asamxlkasmxoamom");
+						CompilationUnit cunit = JavaParser.parse(f);
+						System.out.println("huha");
+						ClassOrInterfaceDeclaration cid = GetClassAndInterface(cunit);
+						System.out.println(cid+"----------");
+						if (cid != null) {
+							System.out.println("cid is not null");
+							CIMap.put(cid.getName().toString(), cid);
+							System.out.println("CIMAP : "+cid.getName().toString());
+						}
+						parseClassInterfaceTypes(cid);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			build_uml.append("\n@enduml");
+            System.out.print("uml " + build_uml.toString());
+		}
+		else
+		{
+			System.out.println("file does not exists");
+		}
+		
+		return build_uml;
+		
+	}
+
+
+	
+	
+
+}
