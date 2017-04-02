73fb9686ee08a1c94c9daf01ae5a9e236cc024ca
Avdeep2802 <Avdeep2802@gmail.com>
2017-04-01 22:36:44 -0700

    constructor code refined

--- UML_Parser_202/src/Const_Parse.java
+++ UML_Parser_202/src/Const_Parse.java
@@ -18,5 +18,27 @@ public class Const_Parse {
     
     public void Constructor_Read(ClassOrInterfaceDeclaration unit) {
         
+        NodeList<BodyDeclaration<?>> list = unit.getMembers();
+        
+        for (BodyDeclaration node : list) {
+            if (node instanceof ConstructorDeclaration) {
+                ConstructorDeclaration l = (ConstructorDeclaration) node;
+                StringBuilder builder = new StringBuilder();
+                for (Parameter params : l.getParameters()) {
+                    builder.append(params.getName()).append(" : ").append(params.getType()).append(" ");
+                    String paramType = params.getType().toString();
+                    if (_mainParser._getTypeMap().containsKey(paramType) &&
+                        (_mainParser._getTypeMap().get(paramType).isInterface())) {
+                        _mainParser.getRelationsBuilder().updateRelationsList(new RelationType(unit.getNameAsString(),
+                                                                                               params.getType().toString(), RelationEnum.DEPENCENCY, " : using "));
+                    }
+                }
+                _mainParser.getUmlBuilder().append("+").append(l.getName())
+                .append('(').append(builder).append(')')
+                .append("\n");
+            }
+            
+        }
+        
     }
 }
