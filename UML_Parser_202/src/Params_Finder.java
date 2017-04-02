f37465c4b7395c822cb2b0b65aaa16afaa0c722b
Avdeep2802 <Avdeep2802@gmail.com>
2017-04-01 22:39:45 -0700

    params function

---
+++ UML_Parser_202/src/Params_Finder.java
@@ -0,0 +1,17 @@
+import com.github.javaparser.ast.type.Type;
+
+/**
+ * Created by kaushik on 1/3/17.
+ */
+public class Params_Finder {
+
+    private void checkRefrenceOrCollectionType(Type attributeType) {
+
+    }
+
+    private void checkIsPrimitiveType(Type attrType) {
+
+    }
+
+
+}
