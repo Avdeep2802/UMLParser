5ef00f59589fc96a2b006c09e13d186b5e57b83c
Avdeep2802 <Avdeep2802@gmail.com>
2017-04-08 17:04:59 -0700

    new function

--- UML_Parser_202/src/Relations.java
+++ UML_Parser_202/src/Relations.java
@@ -1,4 +1,23 @@
 public class RelationType {
+
+	String startClass;
+    String endClass;
+    RelationEnum r_enum;
+    String label;
+
+    RelationType(String startClass, String endClass, RelationEnum r_enum, String label) {
+        this.endClass = endClass;
+        this.startClass = startClass;
+        this.label = label;
+        this.r_enum = r_enum;
+    }
+
+    @Override
+    public String toString() {
+        return endClass + r_enum.symbol + startClass + label;
+    }
+
+    
     
 }
 
