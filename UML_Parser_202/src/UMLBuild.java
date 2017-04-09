cebd5490d67a4808448197c9e5c905470a2f9c5f
Avdeep2802 <Avdeep2802@gmail.com>
2017-04-08 17:14:06 -0700

    full class

--- UML_Parser_202/src/UMLBuild.java
+++ UML_Parser_202/src/UMLBuild.java
@@ -1,3 +1,12 @@
 public class UMLBuild {
+    private StringBuilder _umlGeneratorInput = new StringBuilder("@startuml\n skinparam classAttributeIconSize 0\n");
+    public void addUMLInput(String inputs) {
+        _umlGeneratorInput.append(inputs);
+    }
+
    
+    public StringBuilder getUMLInput() {
+        return _umlGeneratorInput;
+    }
+
 }
