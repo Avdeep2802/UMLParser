5d577c6566b888f9472840c028e54ec29f614cd3
Avdeep2802 <Avdeep2802@gmail.com>
2017-04-15 21:28:47 -0700

    method finder method

--- UML_Parser_202/src/Sequence_Gen.java
+++ UML_Parser_202/src/Sequence_Gen.java
@@ -1,7 +1,18 @@
 
 class Sequence_Gen {
 
-	private void readStartMethodBody(String fileName, String methodName) {
+	private void MethodBody(String file_name, String method_name) {
+		File file = new File(file_name);
+        if (file.exists() && file.getName().endsWith(".java")) {
+            try {
+                CompilationUnit unit = JavaParser.parse(file);
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
 
 	}
 
