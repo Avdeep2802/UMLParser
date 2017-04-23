8812d2cc524f6710c911a416115041158daed5c6
Avdeep2802 <Avdeep2802@gmail.com>
2017-04-22 21:44:09 -0700

    testing

---
+++ UML_Parser_202/src/test2.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+import gen.lib.pathplan.solvers__c;
+import net.sourceforge.plantuml.SourceStringReader;
+
+public class test2 {
+	
+	
+	private static HashMap<String,ClassOrInterfaceDeclaration> class_interface_map;
+	private static StringBuilder build_uml;
+	private static StringBuilder build_relation = new StringBuilder();
+	
+
+	public static StringBuilder readCode() throws FileNotFoundException
+	{
+		class_interface_map = new HashMap<>();
+		build_uml = new StringBuilder();
+		build_uml.append("@startuml").append("\n");
+		
+		String path = "/Users/avdeepsandhu/Desktop/A";
+		File file = new File(path);
+		if(file.exists())
+		{
+			for(File f : file.listFiles())
+			{
+				if(f.getName().endsWith(".java"))
+				{				
+					CompilationUnit cunit = JavaParser.parse(f);
+					ClassOrInterfaceDeclaration class_node = getNodes(cunit);
+					if(class_node != null)
+					{						
+						class_interface_map.put(class_node.getNameAsString(), class_node);
+					}
+				}
+			}
+			
+			for(ClassOrInterfaceDeclaration node: class_interface_map.values())
+			{
+                parseClassInterfaceTypes(node);
+
+			}
+			build_uml.append("@enduml");
+			
+		}		
+		return build_uml;
+	}	
+	
+	
+	
+	
+}
+
